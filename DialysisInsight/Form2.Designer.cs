fe3ee19f3900c0de5ff4ab27f33ec1fa4ab611e3
fhamyla <fhamyla.devera@gmail.com>
2024-11-03 15:11:09 +0800

    menu maximize and minimize

--- DialysisInsight/Form2.Designer.cs
+++ DialysisInsight/Form2.Designer.cs
@@ -368,10 +368,10 @@
             maximize.CustomizableEdges = customizableEdges28;
             maximize.Image = Properties.Resources.maximize;
             maximize.ImageRotate = 0F;
-            maximize.Location = new Point(881, 4);
+            maximize.Location = new Point(871, 4);
             maximize.Name = "maximize";
             maximize.ShadowDecoration.CustomizableEdges = customizableEdges29;
-            maximize.Size = new Size(25, 23);
+            maximize.Size = new Size(27, 23);
             maximize.TabIndex = 7;
             maximize.TabStop = false;
             // 
@@ -382,7 +382,7 @@
             minimize.CustomizableEdges = customizableEdges30;
             minimize.Image = Properties.Resources.window_minimize;
             minimize.ImageRotate = 0F;
-            minimize.Location = new Point(840, -6);
+            minimize.Location = new Point(830, -6);
             minimize.Name = "minimize";
             minimize.ShadowDecoration.CustomizableEdges = customizableEdges31;
             minimize.Size = new Size(35, 30);
