479038c72a9f743f155f4f898e9e80233d3bf3be
fhamyla <fhamyla.devera@gmail.com>
2024-12-02 22:53:31 +0800

    added panel and buttons

--- DialysisInsight/Dashboard.Designer.cs
+++ DialysisInsight/Dashboard.Designer.cs
@@ -28,19 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            calender = new Guna.UI2.WinForms.Guna2Button();
+            addhealthdata = new Guna.UI2.WinForms.Guna2Button();
+            settings = new Guna.UI2.WinForms.Guna2Button();
+            logout = new Guna.UI2.WinForms.Guna2Button();
+            guna2Panel1.SuspendLayout();
+            guna2Panel2.SuspendLayout();
             SuspendLayout();
             // 
             // guna2Panel1
             // 
-            guna2Panel1.CustomizableEdges = customizableEdges1;
+            guna2Panel1.Controls.Add(guna2Panel2);
+            guna2Panel1.CustomizableEdges = customizableEdges11;
             guna2Panel1.Location = new Point(-7, -6);
             guna2Panel1.Name = "guna2Panel1";
-            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges12;
             guna2Panel1.Size = new Size(897, 567);
             guna2Panel1.TabIndex = 0;
+            // 
+            // guna2Panel2
+            // 
+            guna2Panel2.BackColor = Color.FromArgb(234, 234, 234);
+            guna2Panel2.BorderRadius = 10;
+            guna2Panel2.Controls.Add(logout);
+            guna2Panel2.Controls.Add(settings);
+            guna2Panel2.Controls.Add(addhealthdata);
+            guna2Panel2.Controls.Add(calender);
+            guna2Panel2.CustomizableEdges = customizableEdges9;
+            guna2Panel2.Location = new Point(19, 18);
+            guna2Panel2.Name = "guna2Panel2";
+            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2Panel2.Size = new Size(155, 231);
+            guna2Panel2.TabIndex = 0;
+            // 
+            // calender
+            // 
+            calender.BorderRadius = 10;
+            calender.CustomizableEdges = customizableEdges7;
+            calender.DisabledState.BorderColor = Color.DarkGray;
+            calender.DisabledState.CustomBorderColor = Color.DarkGray;
+            calender.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            calender.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            calender.FillColor = Color.FromArgb(64, 64, 64);
+            calender.Font = new Font("Segoe UI", 9F);
+            calender.ForeColor = Color.White;
+            calender.ImageAlign = HorizontalAlignment.Left;
+            calender.Location = new Point(0, 0);
+            calender.Name = "calender";
+            calender.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            calender.Size = new Size(155, 45);
+            calender.TabIndex = 1;
+            calender.Text = "Calender";
+            calender.TextAlign = HorizontalAlignment.Left;
+            // 
+            // addhealthdata
+            // 
+            addhealthdata.BorderRadius = 10;
+            addhealthdata.CustomizableEdges = customizableEdges5;
+            addhealthdata.DisabledState.BorderColor = Color.DarkGray;
+            addhealthdata.DisabledState.CustomBorderColor = Color.DarkGray;
+            addhealthdata.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            addhealthdata.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            addhealthdata.FillColor = Color.FromArgb(64, 64, 64);
+            addhealthdata.Font = new Font("Segoe UI", 9F);
+            addhealthdata.ForeColor = Color.White;
+            addhealthdata.ImageAlign = HorizontalAlignment.Left;
+            addhealthdata.Location = new Point(0, 51);
+            addhealthdata.Name = "addhealthdata";
+            addhealthdata.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            addhealthdata.Size = new Size(155, 45);
+            addhealthdata.TabIndex = 2;
+            addhealthdata.Text = "Health Data";
+            addhealthdata.TextAlign = HorizontalAlignment.Left;
+            // 
+            // settings
+            // 
+            settings.BorderRadius = 10;
+            settings.CustomizableEdges = customizableEdges3;
+            settings.DisabledState.BorderColor = Color.DarkGray;
+            settings.DisabledState.CustomBorderColor = Color.DarkGray;
+            settings.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            settings.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            settings.FillColor = Color.FromArgb(64, 64, 64);
+            settings.Font = new Font("Segoe UI", 9F);
+            settings.ForeColor = Color.White;
+            settings.ImageAlign = HorizontalAlignment.Left;
+            settings.Location = new Point(0, 102);
+            settings.Name = "settings";
+            settings.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            settings.Size = new Size(155, 45);
+            settings.TabIndex = 3;
+            settings.Text = "Settings";
+            settings.TextAlign = HorizontalAlignment.Left;
+            // 
+            // logout
+            // 
+            logout.BorderRadius = 10;
+            logout.CustomizableEdges = customizableEdges1;
+            logout.DisabledState.BorderColor = Color.DarkGray;
+            logout.DisabledState.CustomBorderColor = Color.DarkGray;
+            logout.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            logout.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            logout.FillColor = Color.FromArgb(64, 64, 64);
+            logout.Font = new Font("Segoe UI", 9F);
+            logout.ForeColor = Color.White;
+            logout.ImageAlign = HorizontalAlignment.Left;
+            logout.Location = new Point(0, 186);
+            logout.Name = "logout";
+            logout.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            logout.Size = new Size(155, 45);
+            logout.TabIndex = 4;
+            logout.Text = "Logout";
+            logout.TextAlign = HorizontalAlignment.Left;
             // 
             // Dashboard
             // 
@@ -52,11 +165,18 @@
             Controls.Add(guna2Panel1);
             Name = "Dashboard";
             ShowIcon = false;
+            guna2Panel1.ResumeLayout(false);
+            guna2Panel2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2Button logout;
+        private Guna.UI2.WinForms.Guna2Button settings;
+        private Guna.UI2.WinForms.Guna2Button addhealthdata;
+        private Guna.UI2.WinForms.Guna2Button calender;
     }
 }
