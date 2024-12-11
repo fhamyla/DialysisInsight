9ddf461108cf3e75e21f97a62dec931ca945c308
fhamyla <fhamyla.devera@gmail.com>
2024-12-11 19:52:46 +0800

    adding label and checkbox on user control

--- DialysisInsight/ucDays.Designer.cs
+++ DialysisInsight/ucDays.Designer.cs
@@ -28,17 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            label1 = new Label();
+            checkBox1 = new CheckBox();
+            panel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(checkBox1);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(1, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(119, 98);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Poppins", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(80, 8);
+            label1.Name = "label1";
+            label1.Size = new Size(30, 26);
+            label1.TabIndex = 0;
+            label1.Text = "00";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(12, 13);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(15, 14);
+            checkBox1.TabIndex = 1;
+            checkBox1.UseVisualStyleBackColor = true;
             // 
             // ucDays
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(224, 224, 224);
+            Controls.Add(panel1);
             Name = "ucDays";
+            Padding = new Padding(1);
             Size = new Size(121, 100);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private CheckBox checkBox1;
     }
 }
