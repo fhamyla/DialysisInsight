4dd7fd0f45a312d875c384a993ce566fdc01605a
fhamyla <fhamyla.devera@gmail.com>
2024-12-31 14:08:02 +0800

    added design textboxes

--- DialysisInsight/HealthData.Designer.cs
+++ DialysisInsight/HealthData.Designer.cs
@@ -28,28 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HealthData));
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges27 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges28 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges25 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges26 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges23 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges24 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges21 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges22 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             minmax = new Guna.UI2.WinForms.Guna2Button();
             guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
-            Weight = new Guna.UI2.WinForms.Guna2TextBox();
-            HeartRate = new Guna.UI2.WinForms.Guna2TextBox();
-            BloodSugarLevel = new Guna.UI2.WinForms.Guna2TextBox();
-            BloodPressure = new Guna.UI2.WinForms.Guna2TextBox();
             DialysisSessionsCompleted = new Guna.UI2.WinForms.Guna2TextBox();
+            BloodPressure = new Guna.UI2.WinForms.Guna2TextBox();
+            BloodSugarLevel = new Guna.UI2.WinForms.Guna2TextBox();
+            HeartRate = new Guna.UI2.WinForms.Guna2TextBox();
+            Weight = new Guna.UI2.WinForms.Guna2TextBox();
             guna2Panel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -57,7 +57,7 @@
             // 
             minmax.BackColor = Color.Transparent;
             minmax.BorderRadius = 10;
-            minmax.CustomizableEdges = customizableEdges15;
+            minmax.CustomizableEdges = customizableEdges1;
             minmax.DisabledState.BorderColor = Color.DarkGray;
             minmax.DisabledState.CustomBorderColor = Color.DarkGray;
             minmax.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -70,7 +70,7 @@
             minmax.Location = new Point(864, 3);
             minmax.Name = "minmax";
             minmax.PressedColor = Color.Transparent;
-            minmax.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            minmax.ShadowDecoration.CustomizableEdges = customizableEdges2;
             minmax.Size = new Size(15, 19);
             minmax.TabIndex = 4;
             minmax.Click += minmax_Click;
@@ -82,96 +82,16 @@
             guna2Panel1.Controls.Add(BloodSugarLevel);
             guna2Panel1.Controls.Add(HeartRate);
             guna2Panel1.Controls.Add(Weight);
-            guna2Panel1.CustomizableEdges = customizableEdges27;
+            guna2Panel1.CustomizableEdges = customizableEdges13;
             guna2Panel1.Location = new Point(-6, -7);
             guna2Panel1.Name = "guna2Panel1";
-            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges28;
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges14;
             guna2Panel1.Size = new Size(894, 567);
             guna2Panel1.TabIndex = 5;
             // 
-            // Weight
-            // 
-            Weight.CustomizableEdges = customizableEdges25;
-            Weight.DefaultText = "";
-            Weight.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
-            Weight.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
-            Weight.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
-            Weight.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
-            Weight.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
-            Weight.Font = new Font("Segoe UI", 9F);
-            Weight.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
-            Weight.Location = new Point(55, 73);
-            Weight.Name = "Weight";
-            Weight.PasswordChar = '\0';
-            Weight.PlaceholderText = "";
-            Weight.SelectedText = "";
-            Weight.ShadowDecoration.CustomizableEdges = customizableEdges26;
-            Weight.Size = new Size(200, 36);
-            Weight.TabIndex = 0;
-            // 
-            // HeartRate
-            // 
-            HeartRate.CustomizableEdges = customizableEdges23;
-            HeartRate.DefaultText = "";
-            HeartRate.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
-            HeartRate.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
-            HeartRate.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
-            HeartRate.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
-            HeartRate.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
-            HeartRate.Font = new Font("Segoe UI", 9F);
-            HeartRate.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
-            HeartRate.Location = new Point(332, 108);
-            HeartRate.Name = "HeartRate";
-            HeartRate.PasswordChar = '\0';
-            HeartRate.PlaceholderText = "";
-            HeartRate.SelectedText = "";
-            HeartRate.ShadowDecoration.CustomizableEdges = customizableEdges24;
-            HeartRate.Size = new Size(200, 36);
-            HeartRate.TabIndex = 1;
-            // 
-            // BloodSugarLevel
-            // 
-            BloodSugarLevel.CustomizableEdges = customizableEdges21;
-            BloodSugarLevel.DefaultText = "";
-            BloodSugarLevel.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
-            BloodSugarLevel.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
-            BloodSugarLevel.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
-            BloodSugarLevel.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
-            BloodSugarLevel.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
-            BloodSugarLevel.Font = new Font("Segoe UI", 9F);
-            BloodSugarLevel.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
-            BloodSugarLevel.Location = new Point(513, 203);
-            BloodSugarLevel.Name = "BloodSugarLevel";
-            BloodSugarLevel.PasswordChar = '\0';
-            BloodSugarLevel.PlaceholderText = "";
-            BloodSugarLevel.SelectedText = "";
-            BloodSugarLevel.ShadowDecoration.CustomizableEdges = customizableEdges22;
-            BloodSugarLevel.Size = new Size(200, 36);
-            BloodSugarLevel.TabIndex = 2;
-            // 
-            // BloodPressure
-            // 
-            BloodPressure.CustomizableEdges = customizableEdges19;
-            BloodPressure.DefaultText = "";
-            BloodPressure.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
-            BloodPressure.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
-            BloodPressure.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
-            BloodPressure.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
-            BloodPressure.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
-            BloodPressure.Font = new Font("Segoe UI", 9F);
-            BloodPressure.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
-            BloodPressure.Location = new Point(494, 339);
-            BloodPressure.Name = "BloodPressure";
-            BloodPressure.PasswordChar = '\0';
-            BloodPressure.PlaceholderText = "";
-            BloodPressure.SelectedText = "";
-            BloodPressure.ShadowDecoration.CustomizableEdges = customizableEdges20;
-            BloodPressure.Size = new Size(200, 36);
-            BloodPressure.TabIndex = 3;
-            // 
             // DialysisSessionsCompleted
             // 
-            DialysisSessionsCompleted.CustomizableEdges = customizableEdges17;
+            DialysisSessionsCompleted.CustomizableEdges = customizableEdges3;
             DialysisSessionsCompleted.DefaultText = "";
             DialysisSessionsCompleted.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
             DialysisSessionsCompleted.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
@@ -185,9 +105,92 @@
             DialysisSessionsCompleted.PasswordChar = '\0';
             DialysisSessionsCompleted.PlaceholderText = "";
             DialysisSessionsCompleted.SelectedText = "";
-            DialysisSessionsCompleted.ShadowDecoration.CustomizableEdges = customizableEdges18;
+            DialysisSessionsCompleted.ShadowDecoration.CustomizableEdges = customizableEdges4;
             DialysisSessionsCompleted.Size = new Size(200, 36);
             DialysisSessionsCompleted.TabIndex = 4;
+            // 
+            // BloodPressure
+            // 
+            BloodPressure.CustomizableEdges = customizableEdges5;
+            BloodPressure.DefaultText = "";
+            BloodPressure.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            BloodPressure.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            BloodPressure.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            BloodPressure.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            BloodPressure.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            BloodPressure.Font = new Font("Segoe UI", 9F);
+            BloodPressure.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            BloodPressure.Location = new Point(494, 339);
+            BloodPressure.Name = "BloodPressure";
+            BloodPressure.PasswordChar = '\0';
+            BloodPressure.PlaceholderText = "";
+            BloodPressure.SelectedText = "";
+            BloodPressure.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            BloodPressure.Size = new Size(200, 36);
+            BloodPressure.TabIndex = 3;
+            // 
+            // BloodSugarLevel
+            // 
+            BloodSugarLevel.CustomizableEdges = customizableEdges7;
+            BloodSugarLevel.DefaultText = "";
+            BloodSugarLevel.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            BloodSugarLevel.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            BloodSugarLevel.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            BloodSugarLevel.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            BloodSugarLevel.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            BloodSugarLevel.Font = new Font("Segoe UI", 9F);
+            BloodSugarLevel.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            BloodSugarLevel.Location = new Point(513, 203);
+            BloodSugarLevel.Name = "BloodSugarLevel";
+            BloodSugarLevel.PasswordChar = '\0';
+            BloodSugarLevel.PlaceholderText = "";
+            BloodSugarLevel.SelectedText = "";
+            BloodSugarLevel.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            BloodSugarLevel.Size = new Size(200, 36);
+            BloodSugarLevel.TabIndex = 2;
+            // 
+            // HeartRate
+            // 
+            HeartRate.CustomizableEdges = customizableEdges9;
+            HeartRate.DefaultText = "";
+            HeartRate.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            HeartRate.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            HeartRate.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            HeartRate.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            HeartRate.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            HeartRate.Font = new Font("Segoe UI", 9F);
+            HeartRate.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            HeartRate.Location = new Point(332, 108);
+            HeartRate.Name = "HeartRate";
+            HeartRate.PasswordChar = '\0';
+            HeartRate.PlaceholderText = "";
+            HeartRate.SelectedText = "";
+            HeartRate.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            HeartRate.Size = new Size(200, 36);
+            HeartRate.TabIndex = 1;
+            // 
+            // Weight
+            // 
+            Weight.BorderColor = Color.FromArgb(64, 64, 64);
+            Weight.CustomizableEdges = customizableEdges11;
+            Weight.DefaultText = "";
+            Weight.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            Weight.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            Weight.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            Weight.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            Weight.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            Weight.Font = new Font("Poppins", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Weight.ForeColor = Color.FromArgb(64, 64, 64);
+            Weight.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            Weight.Location = new Point(54, 152);
+            Weight.Margin = new Padding(4, 5, 4, 5);
+            Weight.Name = "Weight";
+            Weight.PasswordChar = '\0';
+            Weight.PlaceholderText = "Enter Weight in Kg";
+            Weight.SelectedText = "";
+            Weight.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            Weight.Size = new Size(229, 72);
+            Weight.TabIndex = 0;
             // 
             // HealthData
             // 
