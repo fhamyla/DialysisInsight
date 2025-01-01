6132a27cdedd3b5dc142ba52b800cc01039984ff
fhamyla <fhamyla.devera@gmail.com>
2025-01-01 19:41:50 +0800

    added backend code for auto resizing

--- DialysisInsight/HealthData.cs
+++ DialysisInsight/HealthData.cs
@@ -15,6 +15,20 @@ namespace DialysisInsight
         private Size formOriginalSize;
         private Rectangle recminmax;
         private Rectangle recpanel1;
+        private Rectangle reckilo;
+        private Rectangle reckilobox;
+        private Rectangle recbpm;
+        private Rectangle recbpmbox;
+        private Rectangle recmg;
+        private Rectangle recmgbox;
+        private Rectangle recmm;
+        private Rectangle recmm1;
+        private Rectangle recmm2;
+        private Rectangle recsessions;
+        private Rectangle recsessions1;
+        private Rectangle recsessions2;
+        private Rectangle recsave;
+        private Rectangle recback;
         public HealthData()
         {
             InitializeComponent();
@@ -22,12 +36,40 @@ namespace DialysisInsight
             formOriginalSize = this.Size;
             recminmax = new Rectangle(minmax.Location, minmax.Size);
             recpanel1 = new Rectangle(guna2Panel1.Location, guna2Panel1.Size);
+            reckilo = new Rectangle(guna2HtmlLabel1.Location, guna2HtmlLabel1.Size);
+            reckilobox = new Rectangle(Weight.Location, Weight.Size);
+            recbpm = new Rectangle(guna2HtmlLabel2.Location, guna2HtmlLabel2.Size);
+            recbpmbox = new Rectangle(HeartRate.Location, HeartRate.Size);
+            recmg = new Rectangle(guna2HtmlLabel3.Location, guna2HtmlLabel3.Size);
+            recmgbox = new Rectangle(BloodSugarLevel.Location, BloodSugarLevel.Size);
+            recmm = new Rectangle(guna2HtmlLabel4.Location, guna2HtmlLabel4.Size);
+            recmm1 = new Rectangle(SYS.Location, SYS.Size);
+            recmm2 = new Rectangle(DIA.Location, DIA.Size);
+            recsessions = new Rectangle(guna2HtmlLabel5.Location, guna2HtmlLabel5.Size);
+            recsessions1 = new Rectangle(SessionComplete.Location, SessionComplete.Size);
+            recsessions2 = new Rectangle(CompleteSession.Location, CompleteSession.Size);
+            recsave = new Rectangle(save.Location, save.Size);
+            recback = new Rectangle(back.Location, back.Size);
         }
 
         private void Dashboard_Resiz(object? sender, EventArgs e)
         {
             resize_Control(minmax, recminmax);
             resize_Control(guna2Panel1, recpanel1);
+            resize_Control(guna2HtmlLabel1, reckilo);
+            resize_Control(Weight, reckilobox);
+            resize_Control(guna2HtmlLabel2, recbpm);
+            resize_Control(HeartRate, recbpmbox);
+            resize_Control(guna2HtmlLabel3, recmg);
+            resize_Control(BloodSugarLevel, recmgbox);
+            resize_Control(guna2HtmlLabel4, recmm);
+            resize_Control(SYS, recmm1);
+            resize_Control(DIA, recmm2);
+            resize_Control(guna2HtmlLabel5, recsessions);
+            resize_Control(SessionComplete, recsessions1);
+            resize_Control(CompleteSession, recsessions2);
+            resize_Control(save, recsave);
+            resize_Control(back, recback);
         }
 
         private void resize_Control(Control c, Rectangle r)
